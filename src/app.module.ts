8019995b49cac375d6fd840179669d2b1dd5689c
soorq <fantastfad830@gmail.com>
2026-04-10 21:14:07 +0300

    chore: resolve merge conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@libs/config';
+import { DatabaseModule } from '@libs/database';
+import { ConfigService } from '@nestjs/config';
+import * as schema from './shared/entities';
 
 @Module({
-    imports: [ConfigModule],
+    imports: [
+        ConfigModule,
+        DatabaseModule.registerAsync({
+            global: true,
+            inject: [ConfigService],
+            useFactory: (cfg: ConfigService) => {
+                return {
+                    schema,
+                    schemaName: cfg.getOrThrow('DB_SCHEMA'),
+                    logging: true,
+                };
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
