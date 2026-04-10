4791c56d5f7cf7904435734b9c3781c0299e543b
soorq <fantastfad830@gmail.com>
2026-04-10 19:07:00 +0300

    chore(drizzle):feat(database): setup database lib / configure drizzle

---
+++ libs/config/src/config.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as path from 'path';
+import { ConfigSchema } from './config.schema';
+import { ZodError } from 'zod/v4';
+
+const validateConfig = (config: Record<string, unknown>) => {
+  try {
+    return ConfigSchema.parse(config);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.group('\nENVIRONMENT_VALIDATION_ERROR\n');
+
+      error.issues.forEach((issue) => {
+        const field = issue.path.join('.') || 'ROOT';
+
+        console.group(`Field: ${field}`);
+        console.error(`Message: ${issue.message}`);
+        console.error(`Code:    ${issue.code.toUpperCase()}`);
+        console.groupEnd();
+        console.error('\n');
+      });
+
+      console.groupEnd();
+
+      throw new Error('Invalid environment configuration');
+    }
+    throw error;
+  }
+};
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(process.cwd(), '.env'),
+      validate: validateConfig,
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
+  exports: [NestConfigModule],
+})
+export class ConfigModule {}
