d29bb73a6f4389ab4ecb6629e8e460b85138079f
Maxim <darlinggbm@gmail.com>
2026-04-10 20:53:04 +0300

    chore: migrate e2e tests configuration to vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,22 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        root: './',
+        environment: 'node',
+        include: ['test/**/*.e2e-spec.ts'],
+        alias: {
+            '@libs/config': path.resolve(__dirname, './libs/config/src'),
+            '@libs/database': path.resolve(__dirname, './libs/database/src'),
+            '@src': path.resolve(__dirname, './src'),
+        },
+    },
+    plugins: [
+        swc.vite({
+            module: { type: 'es6' },
+        }),
+    ],
+});
