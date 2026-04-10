4fb50b5b682edb7e67631a7f980a51647ccc799f
Maksym Berehovyi <108676512+maksberegovoi@users.noreply.github.com>
2026-04-11 00:51:31 +0300

    chore: global reformat and setup prettier ignore rules (#14)

--- libs/bootstrap/src/configs/throttler.ts
+++ libs/bootstrap/src/configs/throttler.ts
@@ -1,9 +1,9 @@
 import type { ThrottlerModuleOptions } from '@nestjs/throttler';
 
 export const DEFAULT_THROTTLER_OPTIONS: ThrottlerModuleOptions = [
-  {
-    ttl: 60000,
-    limit: 100,
-    skipIf: (context) => context.getType() !== 'http',
-  },
+    {
+        ttl: 60000,
+        limit: 100,
+        skipIf: (context) => context.getType() !== 'http',
+    },
 ];
