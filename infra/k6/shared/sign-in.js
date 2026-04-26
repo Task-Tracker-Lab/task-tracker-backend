677279e361b9b444fd4dc468c59e481f0b95b37e
Maxim <darlinggbm@gmail.com>
2026-04-26 21:39:46 +0300

    feat(load-test): move sign-in logic into separate module for reusability

---
+++ infra/k6/shared/sign-in.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function signIn(baseUrl, user) {
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const signInRes = http.post(
+        `${baseUrl}/auth/sign-in`,
+        JSON.stringify({ email: user.email, password: user.password }),
+        Object.assign({}, params, { tags: { name: 'sign-in' } }),
+    );
+
+    const signInToken = signInRes.json().token;
+    const signInCookie = signInRes.cookies.refresh ? signInRes.cookies.refresh[0].value : 'MISSING';
+
+    check(signInRes, {
+        'sign-in: status is 201': (r) => r.status === 201,
+        'sign-in: has access token': (r) => r.json().token !== undefined,
+    });
+
+    return {
+        signInToken,
+        signInCookie,
+    };
+}
