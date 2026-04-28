1e8d90bf55a15a6dd029128e4f3cf1b2f06fb76e
Maxim <darlinggbm@gmail.com>
2026-04-29 00:58:10 +0300

    refactor(load-test): simplify scenarios by using centralized ApiClient

---
+++ infra/k6/shared/get-auth-user.js
@@ -0,0 +1,36 @@
+import { check } from 'k6';
+import { ApiClient } from '../common/client.js';
+
+export default function getAuthUser(user, options = {}) {
+    const client = new ApiClient();
+    const requestOptions = Object.assign({}, options);
+
+    if (!requestOptions.tags) {
+        requestOptions.tags = { name: 'auth-sign-in' };
+    }
+
+    const signInRes = client.post(
+        '/auth/sign-in',
+        {
+            email: user.email,
+            password: user.password,
+        },
+        requestOptions,
+    );
+
+    check(signInRes, {
+        'POST /auth/sign-in has token': (r) => r.json().token !== undefined,
+    });
+
+    const token = signInRes.json().token;
+    const refreshCookie = signInRes.cookies.refresh
+        ? signInRes.cookies.refresh[0].value
+        : 'MISSING';
+
+    return {
+        client: new ApiClient({ token }),
+        token,
+        refreshCookie,
+        signInRes,
+    };
+}
